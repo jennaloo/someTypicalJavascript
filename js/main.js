5b34558b8118c6ed38b76e65abca535549f5a86f
Jenna Murphy <jenna.raye.murphy@gmail.com>
2018-10-17 18:58:32 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,72 @@
+//display the length of all 5 variable types in the console.
+//
+//var myNum = 5;
+//var myChar = "stuff stuff";
+//var myBool = false;
+//var myArray = ["stuff", 5, false, "aslkdfal;s"];
+//var myObject = {
+//    stuff: 1,
+//    morestuff: 2
+//}
+//
+//console.log(myNum.length);
+//console.log(myChar.length);
+//console.log(myBool.length);
+//console.log(myArray.length);
+//console.log(myObject.length);
+
+//reverse an array
+
+//
+//var numArray = [1, 2, 3, 4];
+//
+//var copiedArray = function (someArray) {
+//    var newArray = [];
+//    for (var i = 0; i < someArray.length; i++) {
+//        newArray.push(someArray[i]);
+//    }
+//    return newArray;
+//}
+//
+//var reversedArray = copiedArray(numArray).reverse();
+//console.log(reversedArray);
+//
+////-----------------------------------//
+//var numArray = [1, 2, 3, 4];
+//
+//function copiedArray(someArray) {
+//    var newArray = [];
+//    for (var i = 0; i < someArray.length; i++) {
+//        newArray.push(someArray[i]);
+//    }
+//    return newArray;
+//}
+//
+//var reversedArray = copiedArray(numArray).reverse();
+//console.log(reversedArray);
+
+
+//sorting an array of numbers and an array of strings
+//
+//wordArray = ["aa", "au", "ae"];
+//numArray = [2, 5, 4, 3, 1, 6];
+//
+//wordArray.sort(function (a, b) {
+//    return a - b;
+//})
+
+//push a new item to the end of an array
+//
+//myArray = [{
+//    someprop: 1
+//}, {
+//    someprop: 2
+//}, {
+//    someprop: 3
+//}];
+
+//delete object from the end of an array using pop()
+
+//add a new object to the beginning using unshift
+
+//remove the last object at the beginning of an array of objects using shift()
